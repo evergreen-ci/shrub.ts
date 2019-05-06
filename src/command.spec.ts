5757d3b6b0cfd4615adaa6552593dbb8fee1724a
Robert Guo <robertguo@me.com>
2019-05-06 06:28:12 -0400

    use protobuf version

--- src/command.spec.ts
+++ src/command.spec.ts
@@ -1,13 +1,42 @@
-import 'mocha';
-import { CommandDefinition, ExecutionType } from './command';
-import { jsonReplacer } from './utils';
+import { Task, TaskDependency } from "./shrub_pb";
+
+function replacer(key: string, value: any) {
+
+}
+
+interface anyObj {
+  [key: string]: any;
+}
+
+function jsonReplacer(key: string, value: any) {
+  if (value && typeof value === 'object' && !Array.isArray(value)) {
+    const replacement: anyObj = {};
+    for (const k of Object.getOwnPropertyNames(value)) {
+      let snakeKey = k.split(/(?=[A-Z])/).join('_').toLowerCase();
+      if (Array.isArray(value[k])) {
+        console.log(value[k]);
+        snakeKey = snakeKey.replace(/_list$/, '');
+        replacement[snakeKey] = value[k] as Array<any>;
+      }
+      Object.assign(replacement, {[snakeKey]: value[k]});
+    }
+    return replacement;
+  }
+  return value;
+}
 
 describe('CommandDefinition', () => {
   it('should have required properties', () => {
-    const cmd = new CommandDefinition();
-    cmd.functionName.set('dummyFunctionName');
-    cmd.executionType.set(ExecutionType.Setup);
-    cmd.displayName.set('dummyDisplayName');
-    console.log(JSON.stringify(cmd, jsonReplacer));
+    const a = new Task();
+    a.setName('dummyName');
+    a.setPriority(10);
+
+    let td = new TaskDependency();
+    td.setName('taskDep');
+    td.setVariant('variant');
+    a.addDependsOn(td);
+    a.getEnvMap().set("env", "envVal");
+
+    console.log(JSON.stringify(a.toObject(), jsonReplacer));
   });
 });
